ec50809a1110e2b185a3e9596595fbb9b1503b1e
sanderhs1 <sander@saevik.com>
2023-11-28 11:15:09 +0100

    filter by max price

--- ClientApp/src/app/listings/listings.component.ts
+++ ClientApp/src/app/listings/listings.component.ts
@@ -3,74 +3,95 @@ import { IListing } from './listing';
 import { Router } from '@angular/router';
 import { ListingService } from './listings.service';
 
-
 @Component({
   selector: 'app-listings-component',
   templateUrl: './listings.component.html',
   styleUrls: ['./listings.component.css']
 })
-
 export class ListingsComponent implements OnInit {
   viewTitle: string = 'Table';
   displayImage: boolean = true;
   listings: IListing[] = [];
 
- 
-
   constructor(
     private _router: Router,
-    private _listingService: ListingService) { }
+    private _listingService: ListingService
+  ) { }
 
-  private _listFilter: string = '';
-  get listFilter(): string {
-    return this._listFilter;
+  private _nameFilter: string = '';
+  private _maxPrice: number | null = null;
+
+  get nameFilter(): string {
+    return this._nameFilter;
   }
-  set listFilter(value: string) {
-    this._listFilter = value;
-    console.log('In setter:', value);
-    this.filteredListings = this.performFilter(value);
+
+  set nameFilter(value: string) {
+    this._nameFilter = value;
+    console.log('Name Filter:', value);
+    this.filteredListings = this.performFilter();
+  }
+
+  get maxPrice(): number | null {
+    return this._maxPrice;
+  }
+
+  set maxPrice(value: number | null) {
+    this._maxPrice = value;
+    console.log('Max Price:', value);
+    this.filteredListings = this.performFilter();
   }
 
   deleteListing(listing: IListing): void {
     const confirmDelete = confirm(`Are you sure you want to delete "${listing.Name}"?`);
     if (confirmDelete) {
-      this._listingService.deleteListing(listing.ListingId)
-        .subscribe(
-          (response) => {
-            if (response.success) {
-              console.log(response.message);
-              this.filteredListings = this.filteredListings.filter(i => i !== listing);
-            }
-          },
-          (error) => {
-            console.error('Error deleting listing:', error);
-          });
+      this._listingService.deleteListing(listing.ListingId).subscribe(
+        (response) => {
+          if (response.success) {
+            console.log(response.message);
+            this.filteredListings = this.filteredListings.filter((i) => i !== listing);
+          }
+        },
+        (error) => {
+          console.error('Error deleting listing:', error);
+        }
+      );
     }
   }
 
   getListings(): void {
-    this._listingService.getListings()
-      .subscribe(data => {
-        console.log('All', JSON.stringify(data));
-        this.listings = data;
-        this.filteredListings = this.listings;
-      }
-      );
+    this._listingService.getListings().subscribe((data) => {
+      console.log('All', JSON.stringify(data));
+      this.listings = data;
+      this.filteredListings = this.listings;
+    });
   }
 
   filteredListings: IListing[] = this.listings;
 
-  performFilter(filterBy: string): IListing[] {
-    filterBy = filterBy.toLocaleLowerCase();
-    return this.listings.filter((listing: IListing) =>
-      listing.Name.toLocaleLowerCase().includes(filterBy));
+  performFilter(): IListing[] {
+    let filteredListings = this.listings;
+
+    if (this.nameFilter) {
+      const nameFilter = this.nameFilter.toLocaleLowerCase();
+      filteredListings = filteredListings.filter((listing: IListing) =>
+        listing.Name.toLocaleLowerCase().includes(nameFilter)
+      );
+    }
+
+    if (this.maxPrice !== null) {
+      filteredListings = filteredListings.filter(
+        (listing: IListing) => listing.Price <= this.maxPrice!
+      );
+    }
+
+    return filteredListings;
   }
 
   toggleImage(): void {
     this.displayImage = !this.displayImage;
   }
 
-  navigateToListingform() {
+  navigateToListingform(): void {
     this._router.navigate(['/listingform']);
   }
 
